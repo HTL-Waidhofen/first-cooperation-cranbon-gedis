5faebe82b72c4d5309391e1ff5cf7c9070579a2d
Deiac Gabriel <gabriel.deiac@htlwy.at>
2023-09-22 21:03:41 +0200

    prototyp js done

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function moreInfo(event){
             info="<p><strong>Name:</strong> Gabriel Deiac</p><p><strong>Skills:</strong> ...</p><p><strong>Aufgabe:</strong> </p> <p><strong>Mehr Info:</strong> <a href='deiac.html' target='blank'>Deiac</a></p>";
             break;
         case "forstner":
-            info="<p><strong>Name:</strong> Kevin Forstner</p><p><strong>Skills:</strong> ...</p><p><strong>Aufgabe:</strong> </p> <p><strong>Mehr Info:</strong> <a href='forster.html' target='blank'>Kevoo</a></p>";
+            info="<p><strong>Name:</strong> Kevin Forstner</p><p><strong>Skills:</strong> ...</p><p><strong>Aufgabe:</strong> </p> <p><strong>Mehr Info:</strong> <a href='forstner.html' target='blank'>Kevoo</a></p>";
             break;
         case "berchti":
             info="<p><strong>Name:</strong> Laurenz Berchtold</p><p><strong>Skills:</strong> ...</p><p><strong>Aufgabe:</strong> </p> <p><strong>Mehr Info:</strong> <a href='berchtold.html' target='blank'>Berchti</a></p>";
